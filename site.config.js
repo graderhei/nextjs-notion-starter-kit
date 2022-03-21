a064df930f03784db2203382f8f4ab859ad2cd7d
graderhei <79974094+graderhei@users.noreply.github.com>
2022-03-21 23:52:05 +0800

    fff
    fff

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'eef5df39522c472280ec8be725beef77',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '大笨牛--notion汉化教程',
+  domain: 'hanhua.dabenniu.cc',
+  author: '大笨牛',
 
   // open graph metadata (optional)
   description: 'Example site description',
